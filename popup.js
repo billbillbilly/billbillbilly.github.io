7772d9796c2bf76704ea35464c652f4b44da7d7f
billbillbilly <xiaohaoy@umich.edu>
2022-02-21 01:45:40 -0500

    - slideshow

--- popup.js
+++ popup.js
@@ -1,14 +1,13 @@
 
 var modal = null;
+var thisId = null;
 
 function pop(item) {
   if(modal === null) {
-    var thisId = item.getAttribute("href");
+    thisId = item.getAttribute("href");
     document.getElementById(thisId).style.display = "block";
     modal = true;
   } else {
-    var thisId = item.getAttribute("href");
-    alert(thisId);
     document.getElementById(thisId).style.display = "none";
     modal = null;
   }
