bca0eb81336fece25df911794749cfca71167006
billbillbilly <xiaohaoy@umich.edu>
2023-02-08 22:57:38 -0500

    update

--- paper_reference/citation_app/scripts/citation_detect.js
+++ paper_reference/citation_app/scripts/citation_detect.js
@@ -210,24 +210,7 @@ const setCookie = (cname, cvalue) => {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-const getCookie = (cname) => {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-}
-console.log(getCookie("isDark"));
-if (getCookie("isDark") === "true") {
-    console.log(getCookie("isDark"));
+if (document.cookie === "isDark=true") {
     isDark = true;
     switchMode();
 } else {
